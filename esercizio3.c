ee3d253450a8609afb2c1bea00726e70ea2f3481
PietroAllocca <pietro.allocca@galileiostiglia.edu.it>
2025-11-05 14:59:46 +0000

    // chiesti tre numeri e memorizzarli
    int main () {
        int num_caramelle, num_amici, resto, quoziente;
        printf("Inserisci num_caramelle: ");
        scanf("%d", &num_caramelle);
        printf("Inserisci num_amici: ");
        scanf("%d", &num_amici);
        quoziente = num_caramelle / num_amici;
        resto = num_caramelle % num_amici;
        printf("Il numero di caramelle a testa è: %d\n", quoziente);
        printf("il numero di caramelle rimaste è: %d", resto);
     }

--- esercizio3.c
+++ esercizio3.c
@@ -4,16 +4,11 @@
 int main () {
     int num_caramelle, num_amici, resto, quoziente; 
     printf("Inserisci num_caramelle: ");
-    
     scanf("%d", &num_caramelle);
-    
     printf("Inserisci num_amici: ");
-    
     scanf("%d", &num_amici);
-
     quoziente = num_caramelle / num_amici;
     resto = num_caramelle % num_amici;
-    
     printf("Il numero di caramelle a testa è: %d\n", quoziente);
     printf("il numero di caramelle rimaste è: %d", resto);
  }
